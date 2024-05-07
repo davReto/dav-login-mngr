c9d49e518a1c9cbd0310b55e03667870a3ec4f7b
Santiago Lopez Amaya <santiagolopezamaya@hotmail.com>
2024-05-07 11:41:07 -0500

    :construction: otp injectable

--- src/application/strategies/otp-login.strategy.ts
+++ src/application/strategies/otp-login.strategy.ts
@@ -1,6 +1,8 @@
+import { Injectable } from '@nestjs/common';
 import { AuthenticationStrategy } from '../../domain/interfaces/authentication.interface';
 import { OtpDto } from '../../domain/dto/otp.dto';
 
+@Injectable()
 export class OtpLoginStrategy implements AuthenticationStrategy {
   async login(loginParams: OtpDto): Promise<boolean> {
     if (loginParams.otp === '123456') {
