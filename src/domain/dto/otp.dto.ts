d396e67743c89ba260727c1d8490ed8127658fce
Santiago Lopez Amaya <santiagolopezamaya@hotmail.com>
2024-05-07 12:19:58 -0500

    :pencil: otp

--- src/domain/dto/otp.dto.ts
+++ src/domain/dto/otp.dto.ts
@@ -1,4 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class OtpDto {
+  @ApiProperty({ description: 'The OTP of the user.', example: '123456' })
   otp: string;
+  @ApiProperty({ description: 'The user id.', example: '123456' })
   userId: string;
 }
